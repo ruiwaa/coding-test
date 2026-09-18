582816cb1b6366809c58aca56a4f62aa56174277
ruiwaa <yeejii08@gmail.com>
2026-09-18 11:00:19 +0900

    feat: 코테 day 24 문제 풀이 완료

---
+++ level-0/26.09.18/치킨.js
@@ -0,0 +1,27 @@
+/* 
+프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 
+쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 
+서비스 치킨에도 쿠폰이 발급됩니다.
+ 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 
+ solution 함수를 완성해주세요.
+*/
+
+function solution(chicken) {
+  var answer = 0;
+  let coupon = chicken;
+
+  while (coupon >= 10) {
+    // 서비스 받을 마리 수 계산
+    const service = Math.floor(coupon / 10);
+    answer += service;
+
+    // 10으로 나눈 쿠폰의 값 재할당
+    // while 조건에 만족할때까지 재할당
+    coupon = (coupon % 10) + service;
+    console.log(coupon);
+  }
+
+  return answer;
+}
+
+solution(1081);
